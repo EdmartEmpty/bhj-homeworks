4e2586cc0034a7d2487a7979dd3c68e08319812f
Edmart <edmart666@gmail.com>
2025-06-19 15:17:46 +0300

    Revert "o"
    This reverts commit 0f742a779c8b28ca4ee4d239d0056b968a12c994.

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -5,31 +5,10 @@ let linkColl = document.querySelectorAll(".dropdown__link");
 
 
 dropValue.forEach((el,index) => el.addEventListener("click", function(e) {
-	
-	dropList[index].classList.toggle("dropdown__list_active");
-	}));
-
-linkColl.forEach((link) => link.addEventListener("click",(event) => {
-		event.preventDefault();
-	
-		console.log();
-	}))
-// for (let i= 0; i<dropValue.length; i++){dropValue[i].onclick = (e) => {
-//     let target = e.target;
-// 	 dropList[i].classList.toggle("dropdown__list_active");
-	 
-
-// 	[...linkColl].forEach((el)=> el.onclick = (e) => {
-// 		e.preventDefault();
-		
-// 		let link = e.target;
-// 		target.textContent = link.textContent;
-// 		dropList[i].classList.remove("dropdown__list_active");
-// }
-// 	);
     
-// }}
-
+    dropList[index].classList.toggle("dropdown__list_active");
+	
+    }));
 
 	linkColl.forEach((link) => link.addEventListener("click", function(event) {
 		
