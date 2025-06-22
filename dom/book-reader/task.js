25735522823dbe4b27d183bf917a015913fe6d81
EdmartEmpty <edmart666@gmail.com>
2025-06-22 12:33:51 +0300

    Обнаружил метод startWith
    Добавил метод StartWith для удаления определенного класса

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,76 +1,49 @@
-let book = document.querySelector(".book__content");
+let colorColl = document.querySelectorAll(".color");
+let bookContent = document.querySelector(".book__content");
 let bookSize = document.querySelectorAll(".font-size");
- let colorColl = document.querySelectorAll(".color");
- 
- 
- 
+
+
 bookSize.forEach((elem) => elem.addEventListener("click", (e) => {
     e.preventDefault();
-    if(elem.dataset.size === "small"){
+    if (elem.dataset.size === "small") {
         book.classList.add("book_fs-small");
         book.classList.remove("book_fs-big");
-    } else if(elem.dataset.size === "big"){
+    } else if (elem.dataset.size === "big") {
         book.classList.add("book_fs-big");
         book.classList.remove("book_fs-small");
     } else if (elem.dataset.size !== "big" && elem.dataset.size !== "small") {
-        book.classList.remove("book_fs-big");
-        book.classList.remove("book_fs-small");
+        book.classList.remove("book_fs-big","book_fs-small");
     }
-     }));
+}));
 
 
-     colorColl.forEach((elem,i,arr) => elem.addEventListener("click", (e)=> {
-        e.preventDefault();
-        console.log(elem.dataset.textColor);
-        if(elem.classList.contains("text_color_black")){
-            book.classList.remove("book_color-gray","book_color-whitesmoke");
-            book.classList.add("book_color-black");
-             
-            elem.classList.add("color_active");
-            arr[1].classList.remove("color_active");
-            arr[2].classList.remove("color_active");
-        } 
-         else if (elem.classList.contains("text_color_gray")){
-            book.classList.remove("book_color-black","book_color-whitesmoke");
-            book.classList.add("book_color-gray");
-             
-            elem.classList.add("color_active");
-            arr[0].classList.remove("color_active");
-            arr[2].classList.remove("color_active");
+colorColl.forEach((el) => el.addEventListener("click", (e) => {
+    e.preventDefault();
+    let div = el.closest("div");
+
+
+    div.querySelectorAll(".color").forEach((i) => i.classList.remove("color_active"));
+
+    for (let fromDiv of div.querySelectorAll(".color")) {
+        if (fromDiv === el) {
+            fromDiv.classList.add("color_active");
+            if (e.target.classList.contains("text_color_" + e.target.dataset.textColor)) {
+               Array.from(bookContent.classList).forEach((className) => {
+                if(className.startsWith("book_color-")){
+                    bookContent.classList.remove(className);
+                }})
+                bookContent.classList.add("book_color-" + e.target.dataset.textColor);
+
+            }
+            else {
+                Array.from(bookContent.classList).forEach((className) => {
+                    if(className.startsWith("book_bg-")){
+                        bookContent.classList.remove(className);
+                    }})
+                bookContent.classList.add("book_bg-" + e.target.dataset.bgColor);
+
+            };
         }
-        else if (elem.classList.contains("text_color_whitesmoke")){
-            book.classList.remove("book_color-black","book_color-gray");
-            book.classList.add("book_color-whitesmoke");
-             
-            elem.classList.add("color_active");
-            arr[0].classList.remove("color_active");
-            arr[1].classList.remove("color_active");
-        }
-         else if(elem.classList.contains("bg_color_black")){
-            book.classList.remove("book_bg-whitesmoke","book_bg-gray");
-            book.classList.add("book_bg-black");
+    }
 
-            elem.classList.add("color_active");
-
-            arr[4].classList.remove("color_active");
-            arr[5].classList.remove("color_active");
-        } 
-         else if (elem.classList.contains("bg_color_gray")){
-            book.classList.remove("book_bg-whitesmoke","book_bg-black");
-            book.classList.add("book_bg-gray");
-
-            elem.classList.add("color_active");
-            arr[3].classList.remove("color_active");
-            arr[5].classList.remove("color_active");
-        }
-        else if (elem.classList.contains("bg_color_white")){
-            book.classList.remove("book_bg-black","book_bg-gray");
-            book.classList.add("book_bg-bg-whitesmoke");
-
-            elem.classList.add("color_active");
-            arr[3].classList.remove("color_active");
-            arr[4].classList.remove("color_active");
-        }
-        
-
-     }))
+}));
