6368c5742d098a3069133a132900e47237b858da
EdmartEmpty <edmart666@gmail.com>
2025-06-23 15:24:36 +0300

    Доп задание к банерам на скорость
    Тут не чем гордиться , надо было просто очищать созданный интервал и создавать новый и все.

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     
     let cards = document.querySelectorAll(".card");
-   
+  let timers = [];
     let start = Array.from(cards).map(() => 0);
 
     function switcher (){
@@ -11,15 +11,19 @@ document.addEventListener("DOMContentLoaded", () => {
         let row = el.querySelectorAll(".rotator__case");
         row.forEach((e) => e.classList.remove("rotator__case_active"));
         
-          row[start[index]].style.color = row[start[index]].dataset.color;
-            row[start[index]].classList.add("rotator__case_active");
-             
-      
-         
-        start[index] < cards[0].querySelectorAll(".rotator__case").length - 1?  ++start[index] : start[index] = 0;
-      
-    
+        row[start[index]].style.color = row[start[index]].dataset.color;
+        let speed =  row[start[index]].dataset.speed;
+        
+        row[start[index]].classList.add("rotator__case_active");
+        
+        start[index] = (start[index]+1)% row.length;
+
+        if (timers[index]) {
+            clearTimeout(timers[index]);
+        }
+
+       timers[index] = setTimeout(switcher,speed);
     })}
     ;
-    setInterval(switcher,1000);
+  switcher();
 })
